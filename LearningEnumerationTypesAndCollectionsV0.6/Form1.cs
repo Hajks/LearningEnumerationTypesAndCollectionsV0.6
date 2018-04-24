dfd132cd3ce4befa19efe0ff689a77eea1f6a266
Hajk <tunczorx@gmail.com>
2018-04-24 13:06:07 +0200

    Task from page 399, from "Head first C#".

--- LearningEnumerationTypesAndCollectionsV0.6/Form1.cs
+++ LearningEnumerationTypesAndCollectionsV0.6/Form1.cs
@@ -10,11 +10,53 @@ using System.Windows.Forms;
 
 namespace LearningEnumerationTypesAndCollectionsV0._6
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form // It was simple task, to arrange code in the right order. Code isn't readable at all, it's not my fault, i've used what was given. Task from page 399, from "Head first C#". 
     {
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> a = new List<string>();
+
+            string zilch = "zero";
+            string first = "jeden";
+            string second = "dwa";
+            string third = "trzy";
+            string fourth = "4.2";
+            string twopointwo = "2.2";
+
+            a.Add(zilch);
+            a.Add(first);
+            a.Add(second);
+            a.Add(third);
+
+            if(a.Contains("trzy"))
+            {
+                a.Add("cztery");
+            }
+            if(a.IndexOf("Cztery") != 4)
+            {
+                a.Add(fourth);
+            }
+            a.RemoveAt(2);
+            if(a.Contains("dwa"))
+            {
+                a.Add(twopointwo);
+            }
+            printL(a);
+        }
+        private void printL(List<string> a)
+        {
+            string result = "";
+            foreach (string element in a)
+            {
+                result += "\n" + element;
+            }
+            MessageBox.Show(result);
+        }
+
     }
 }
